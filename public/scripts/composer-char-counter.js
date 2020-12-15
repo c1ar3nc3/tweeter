be04e799d614bead42327c713fb2a0b2d3279d95
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-15 23:53:30 +0000

    added interactive counter that reacts to char count accordingly

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  let maxLength = 140;
+  $('textarea').keyup(function() {
+    let counter = $(this).closest(".new-tweet").find(".counter");
+    let textLength = $(this).val().length;
+    let charsLeft = maxLength - textLength;
+    counter.text(charsLeft);
+    if (charsLeft < 0) {
+      counter.addClass("negativeChar");
+    } else {
+      counter.removeClass("negativeChar");
+    }
+  })
+});
