8057aadcecf0d575a84cdd12b99094a810f8138e
Clarence Williams <xwilliams.clarencex@gmail.com>
2020-12-17 03:56:19 +0000

    added Form Validation w/ alert pop-ups

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,34 +5,34 @@
  */
 
  //test code, will eventually pull tweetData from server
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const tweetData = [
+  // {
+  //   "user": {
+  //     "name": "Newton",
+  //     "avatars": "https://i.imgur.com/73hZDYK.png"
+  //     ,
+  //     "handle": "@SirIsaac"
+  //   },
+  //   "content": {
+  //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   },
+  //   "created_at": 1461116232227
+  // },
+  // {
+  //   "user": {
+  //     "name": "Descartes",
+  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //     "handle": "@rd" },
+  //   "content": {
+  //     "text": "Je pense , donc je suis"
+  //   },
+  //   "created_at": 1461113959088
+  // }
+// ]
 
 
 $(document).ready(() => {
-
+  //creates $tweet HTML data from "/tweet/" database
   const createTweetElement = tweetData => {
     const { userName, avatars, handle, content, createdAt } = tweetData;
     let $tweet = $(`
@@ -56,6 +56,9 @@ $(document).ready(() => {
     return $tweet;
   };
 
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
   const renderTweets = tweetArray => {
     for (const post of tweetArray) {
       let newTweet = createTweetElement({
@@ -68,24 +71,39 @@ $(document).ready(() => {
       $(".all-tweets").append(newTweet);
     }
   };
-  renderTweets(tweetData);
 
+  //will POST tweet to /tweets/ database as an array
+  //will give error messages if tweet is:  empty  || > 140 chars
   $("form").on("submit", function (event) {
     event.preventDefault();
-    const formData = $(this).serialize();
-    $.post("/tweets/", formData)
-      .then (function() {
-        $.getJSON(`/tweets`)
-        .then (data => {
-          const newTweetArray = [];
-          const currentTweet = data[data.length -1];
-          newTweetArray.push(currentTweet);
-          renderTweets(newTweetArray);
-        })
-      })
-    
+    if ($('#tweet-text').val().length > 140) {
+      alert("Max characters reached!")
+    } else if ($('#tweet-text').val().length < 1) {
+      alert("Tweet cannot be empty!")
+    } else {
+      const formData = $(this).serialize();
+      $.post("/tweets/", formData)
+        .then (function() {
+          $.getJSON("/tweets")
+          .then (data => {
+            const newTweetArray = [];
+            const currentTweet = data[data.length -1];
+            newTweetArray.push(currentTweet);
+            renderTweets(newTweetArray);
+          })
+        }
+      )
+    }
   });
 
+  //GET to /tweets/, render new tweet to webpage
+  const loadTweets = function() {
+    $.get("/tweets/", function (data) {
+      renderTweets(data);
+    })
+  };
+
+  loadTweets();
 
 });
 
